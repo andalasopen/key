6332b5adc0f695b80582ec9601dcf9ce22fbb769
andalasopen <53542593+andalasopen@users.noreply.github.com>
2019-08-25 02:57:07 +0700

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+##setup command=wget http://tunisia-dreambox.info/TSplugins/AddKey/installer.sh -O - | /bin/sh
+
+version=3.0
+description='NEW [Add option to enable/Disable check update]'
+
+# remove old version
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/AddKey
+rm -rf /usr/lib/enigma2/python/Plugins/Extensions/Biscotto
+
+# Download and install plugin
+
+cd /tmp 
+set -e
+wget "https://www.mediafire.com/folder/y69rv0b9v2y26/AddKey/AddKey-"$version".tar.gz"
+
+tar -xzf AddKey-"$version".tar.gz -C /
+set +e
+rm -f AddKey-"$version".tar.gz
+cd ..
+
+sync
+echo "#########################################################"
+echo "#            AddKey INSTALLED SUCCESSFULLY              #"
+echo "#                 Raed  &  mfaraj57                     #"              
+echo "#                     support                           #"
+echo "#   https://www.tunisia-sat.com/forums/threads/3955125/ #"
+echo "#########################################################"
+echo "#           your Device will RESTART Now                #"
+echo "#########################################################"
+sleep 3
+if [ -f /var/lib/dpkg/status ]; then
+    init 4 && init 5
+else
+    init 4 && init 3
+fi
+exit 0
